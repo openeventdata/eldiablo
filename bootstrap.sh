21e1ed817766384a3bbd501641a70342ffd3498f
John Beieler <john.b30@gmail.com>
2014-03-21 16:29:37 -0400

    Download and compile TABARI.

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,6 +9,12 @@ echo "Installing base packages..."
 sudo apt-get install git <<-EOF
 yes
 EOF
+sudo apt-get install g++ <<-EOF
+yes
+EOF
+sudo apt-get install libncurses5-dev <<-EOF
+yes
+EOF
 sudo apt-get install unzip
 sudo apt-get install libxml2-dev <<-EOF
 yes
@@ -29,6 +35,14 @@ sudo git clone https://github.com/openeventdata/scraper.git
 
 echo "Installing Python dependencies..."
 sudo pip install -r scraper/requirements.txt
+sudo pip install -r phoenix_pipeline/requirements.txt
+
+echo "Download and compile TABARI..."
+wget http://eventdata.parusanalytics.com/tabari.dir/TABARI.0.8.4b2.make.dir.zip
+unzip TABARI.0.8.4b2.make.dir.zip
+cd TABARI.0.8.4b2.make.dir.zip
+make
+cd
 
 echo "Downloading NLTK data..."
 mkdir -p nltk_data/tokenizers
